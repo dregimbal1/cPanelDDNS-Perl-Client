1c5e6520d8f5cdbc44440124e43b7f37e9be5221
Regimbal93 <regimbal93@gmail.com>
2016-05-10 23:18:57 -0400

    0.0.1 - Support for multiple accounts. Updated cPanel API 2 URI

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
-cPanel Update IP
-================
+# cPanel Dynamic DNS Client
+Just like NO-IP you can use this script on your local machine and when your public IP address changes it will reflect in your A record within cPanel. You do not need root access to the server to run this script. Supports updating the A Record to multiple domain names.
+### Fully Functional!
+Updated to the latest cPanel API 2 URI calls this script will run as much as you like and it will keep track of your public IP and update it accordingly. Once your IP does change this script will remove the old A Record prior to adding the new one. It is self-sufficent. Please star / follow if you find this useful.
+### Version
+0.0.1
+### Installation
+  - Download and unzip
+  - Modify config.cPanelDDNS.txt (example provided)
+	- 1 account per line in the format [cpanel url omit port number] [subdomain] [domain] [username] [password]
+```sh
+$ perl cpanel_update_ip.pl
+```
+License
+----
+Forked from https://github.com/hwa107/cpanel-update-ip
 
-Description
------------
-
-This plugin will login into cPanel and update the A record of the specified domain/subdomain to the machine that's running this script, making it act like a DDNS.
-
-Instruction
------------
-
-The script is by mean to run on a cronjob. But if you want to manually run the script, you can just call `perl cpanel_update_ip.pl` in the command line.
+GNU GENERAL PUBLIC LICENSE
 
 Changelog
----------
-
-v1.0.0
-
-- Initial release
+----
+0.0.1
+  - Support for multiple domain names
+  - Updated to latest cPanel API 2 URI
--- cpanel_update_ip.pl
+++ cpanel_update_ip.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl
 # -------------------------------------------------------------------------------
 # cpanel_update_ip.pl
+# origin author: hwa107 <https://github.com/hwa107>
+# contributing author: David Regimbal <regimbal.me>
 # More information please have a look at README.md
 
 #--- Required -------------------------------------------------------------------
@@ -8,49 +10,70 @@ use strict;
 use LWP::UserAgent;
 use MIME::Base64;
 use XML::Simple;
+use File::Slurp;
 
-#--- Configuration --------------------------------------------------------------
-my $cpanel = "";    # URL for cPanel
-my $hostname = "";  # URL for host
-my $domain = "";    # URL for domain/subdomain to update
-my $user = "";      # cPanel username
-my $pass = "";      # cPanel password
+#--- Configuration -- Do not edit ----------------------------------------------
+
+my $accounts_file = "config.cPanelDDNS.txt"; # path to file (rel to pl file)
+my $cpanel;        # URL for cPanel
+my $subdomain;     # subdomain name
+my $domain;        # example.com
+my $user;          # cPanel username
+my $pass;          # cPanel password
+
+#--- Do not edit the below variables --------------------------------------------
+my $auth;
+my $ua;
+
+#--- Get Accounts on file -------------------------------------------------------
+sub getAccounts {
+  open (my $fh, "<", $accounts_file);
+  my @file_array;
+  while (my $line = <$fh>) {
+      chomp $line;
+      my @line_array = split(/\s+/, $line);
+      push (@file_array, \@line_array);
+  }
+  return @file_array;
+}
 
 #--- Authentication -------------------------------------------------------------
-my $auth = "Basic " . MIME::Base64::encode( $user . ":" . $pass );
-my $ua = LWP::UserAgent->new(ssl_opts => {verify_hostname => 0});
+sub setAuthentication()
+{
+  $auth = "Basic " . MIME::Base64::encode( $user . ":" . $pass );
+  $ua = LWP::UserAgent->new(ssl_opts => {verify_hostname => 0});  
+}
 
-# --- Get domain XML ------------------------------------------------------------
 sub getDomainHash {
-    my $xml = new XML::Simple;
-    my $request = HTTP::Request->new(GET => "https://$cpanel:2083/xml-api/cpanel?cpanel_xmlapi_module=ZoneEdit&cpanel_xmlapi_func=fetchzone&domain=$hostname");
-    $request->header(Authorization => $auth);
-    my $response = $ua->request($request);
-    if ($response->is_success) {
-        my $zone = $xml->XMLin($response->content);
-        if ($zone->{'data'}->{'status'} eq "1") {
-            my $count = @{$zone->{'data'}->{'record'}};
-            my $paramdomain = $domain.".";
-            for (my $item=0; $item<=$count; $item++) {
-                my $name = $zone->{'data'}->{'record'}[$item]->{'name'};
-                my $type = $zone->{'data'}->{'record'}[$item]->{'type'};
-                if (($name eq $paramdomain) && ($type eq "A")) {
-                    return $zone->{'data'}->{'record'}[$item];
-                }
-            }
-        }
-    }
-    return 0;
+  my $xml = new XML::Simple;
+  my $request = HTTP::Request->new(GET => "https://$cpanel:2083/xml-api/cpanel?cpanel_xmlapi_module=ZoneEdit&cpanel_xmlapi_func=fetchzone&domain=$domain");
+  $request->header(Authorization => $auth);
+  my $response = $ua->request($request);
+  if ($response->is_success) {
+      my $zone = $xml->XMLin($response->content);
+      if ($zone->{'data'}->{'status'} eq "1") {
+          my $count = @{$zone->{'data'}->{'record'}};
+          my $paramdomain = $subdomain.".".$domain.".";
+          for (my $item=0; $item<=$count; $item++) {
+              my $name = $zone->{'data'}->{'record'}[$item]->{'name'};
+              my $type = $zone->{'data'}->{'record'}[$item]->{'type'};
+              if (($name eq $paramdomain) && ($type eq "A")) {
+                return $zone->{'data'}->{'record'}[$item];
+              }
+          }
+      }
+  }
+  return 0; 
 }
 
 # --- Get line number -----------------------------------------------------------
 sub getLineNumberA {
-    return $_[0]->{'line'};
+  return $_[0]->{'line'};
 }
 
 # --- Get old IP ----------------------------------------------------------------
 sub getOldIp {
-    return $_[0]->{'address'};
+  return $_[0]->{'address'};
 }
 
 # --- Get server Internet IP ----------------------------------------------------
@@ -68,35 +91,60 @@ sub getNewIp {
 sub setIp {
     my $linenumber = $_[0];
     my $newip = $_[1];
-    my $paramdomain = $domain.".";
-
     my $xml = new XML::Simple;
-    my $request = HTTP::Request->new(GET => "https://$cpanel:2083/xml-api/cpanel?cpanel_xmlapi_module=ZoneEdit&cpanel_xmlapi_func=edit_zone_record&domain=$paramdomain&line=$linenumber&address=$newip");
+    my $request = HTTP::Request->new(GET => "https://$cpanel:2083/xml-api/cpanel?cpanel_xmlapi_user=regimbal&cpanel_xmlapi_module=ZoneEdit&cpanel_xmlapi_func=edit_zone_record&domain=$domain&name=$subdomain&line=$linenumber&address=$newip");
+    
     $request->header(Authorization => $auth);
     my $response = $ua->request($request);
     my $reply = $xml->XMLin($response->content);
-    print $reply->{'data'}->{'statusmsg'}."\n".$response->content."\n";
     if ($reply->{'data'}->{'status'} eq "1") {
+        print STDOUT "Update success!";
         return 1;
+    }
+    else
+    {
+      print STDOUT $reply->{'data'}->{'statusmsg'};
     }
     return 0;
 }
 
 # --- Init ----------------------------------------------------------------------
 sub init {
+  
+  my @accounts = getAccounts();
+  
+  foreach my $i ( 0 .. $#accounts )
+  {
+    #--- Configuration --------------------------------------------------------------
+    $cpanel = $accounts[$i][0];        # URL for cPanel
+    $subdomain = $accounts[$i][1];     # subdomain name
+    $domain = $accounts[$i][2];        # example.com
+    $user = $accounts[$i][3];          # cPanel username
+    $pass = $accounts[$i][4];          # cPanel password
+    
+    print STDOUT "Starting check for $domain \n";
+    
+    setAuthentication();
+    
     my $domain_hash = getDomainHash();
+  
     if (!$domain_hash ) {
-        die("Error");
+        print STDERR "Could not make a connection to host\n";
     } else {
         my $old_ip = getOldIp($domain_hash);
         my $new_ip = getNewIp();
-
+    
         if ($old_ip ne $new_ip) {
             setIp(getLineNumberA($domain_hash), $new_ip);
         }
+        else
+        {
+          print STDOUT "Your IP has not changed since last checked\n";
+        }
     }
+  }
+
 }
 
 # --- Main ----------------------------------------------------------------------
 init();
-
